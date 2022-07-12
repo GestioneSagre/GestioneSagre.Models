d21f06afbb4535d5a76a421e78139fe9b1a197e8
Angelo Pirola <angelo9871@gmail.com>
2022-07-12 23:12:30 +0200

    Aggiunti campi per gestire il destinatario

--- src/GestioneSagre.Models/InputModels/InvioEmail/InputMailSender.cs
+++ src/GestioneSagre.Models/InputModels/InvioEmail/InputMailSender.cs
@@ -4,6 +4,8 @@ public class InputMailSender
 {
     public string MittenteNominativo { get; set; }
     public string MittenteEmail { get; set; }
+    public string DestinatarioNominativo { get; set; }
+    public string DestinatarioEmail { get; set; }
     public string Oggetto { get; set; }
     public string Messaggio { get; set; }
 }
