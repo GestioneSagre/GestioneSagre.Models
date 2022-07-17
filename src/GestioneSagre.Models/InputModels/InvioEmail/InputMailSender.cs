3188d5a140085018638331039a48d49549fffce5
Angelo Pirola <angelo9871@gmail.com>
2022-07-17 23:21:18 +0200

    Applicata proprietà REQUIRED - InputMailSender

--- src/GestioneSagre.Models/InputModels/InvioEmail/InputMailSender.cs
+++ src/GestioneSagre.Models/InputModels/InvioEmail/InputMailSender.cs
@@ -1,11 +1,24 @@
-﻿namespace GestioneSagre.Models.InputModels.InvioEmail;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestioneSagre.Models.InputModels.InvioEmail;
 
 public class InputMailSender
 {
+    [Required]
     public string MittenteNominativo { get; set; }
+
+    [Required]
     public string MittenteEmail { get; set; }
+
+    [Required]
     public string DestinatarioNominativo { get; set; }
+
+    [Required]
     public string DestinatarioEmail { get; set; }
+
+    [Required]
     public string Oggetto { get; set; }
+
+    [Required]
     public string Messaggio { get; set; }
 }
