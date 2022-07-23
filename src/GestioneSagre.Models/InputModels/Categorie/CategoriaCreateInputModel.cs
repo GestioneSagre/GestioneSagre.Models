9ee82594b91c1ad051b4e7287d7632badd2fda7b
Angelo Pirola <angelo9871@gmail.com>
2022-07-23 17:07:29 +0200

    Aggiunto InputModels Categorie

---
+++ src/GestioneSagre.Models/InputModels/Categorie/CategoriaCreateInputModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GestioneSagre.Models.InputModels.Categorie;
+public class CategoriaCreateInputModel
+{
+    public string CategoriaVideo { get; set; }
+    public string CategoriaStampa { get; set; }
+    public string GuidFesta { get; set; }
+}
---
+++ src/GestioneSagre.Models/InputModels/Categorie/CategoriaDeleteInputModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GestioneSagre.Models.InputModels.Categorie;
+public class CategoriaDeleteInputModel
+{
+    public string CategoriaVideo { get; set; }
+    //public string CategoriaStampa { get; set; }
+    public string GuidFesta { get; set; }
+}
