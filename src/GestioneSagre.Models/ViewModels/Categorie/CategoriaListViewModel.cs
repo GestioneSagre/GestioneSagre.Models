67f5e8e43977fecb9e136b8f2c6d34031d9937da
Angelo Pirola <angelo9871@gmail.com>
2022-07-23 17:07:42 +0200

    Aggiunto ViewModels Categorie

---
+++ src/GestioneSagre.Models/ViewModels/Categorie/CategoriaListViewModel.cs
@@ -0,0 +1,8 @@
+﻿using GestioneSagre.Models.ViewModels.Categorie;
+
+namespace GestioneSagre.Models.ViewModels.Feste;
+
+public class CategoriaListViewModel
+{
+    public ListViewModel<CategoriaViewModel> Categorie { get; set; }
+}
---
+++ src/GestioneSagre.Models/ViewModels/Categorie/CategoriaViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GestioneSagre.Infrastructure.Entities;
+using GestioneSagre.Models.ViewModels.Common;
+
+namespace GestioneSagre.Models.ViewModels.Categorie;
+public class CategoriaViewModel : BaseViewModel
+{
+    public string CategoriaVideo { get; set; }
+    public string CategoriaStampa { get; set; }
+    public string GuidFesta { get; set; }
+
+    public static CategoriaViewModel FromEntity(CategoriaEntity categoria)
+    {
+        return new CategoriaViewModel
+        {
+            Id = categoria.Id,
+            CategoriaVideo = categoria.CategoriaVideo,
+            CategoriaStampa = categoria.CategoriaStampa,
+            GuidFesta = categoria.GuidFesta,
+        };
+    }
+}
