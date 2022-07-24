1b31513af7407ed7b8aea845b271fef488323cf0
Angelo Pirola <angelo9871@gmail.com>
2022-07-24 10:27:58 +0200

    Aggiunte proprietà navigazione Categorie

--- src/GestioneSagre.Models/ViewModels/Categorie/CategoriaDetailViewModel.cs
+++ src/GestioneSagre.Models/ViewModels/Categorie/CategoriaDetailViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using GestioneSagre.Infrastructure.Entities;
 using GestioneSagre.Models.ViewModels.Common;
+using GestioneSagre.Models.ViewModels.Prodotti;
 
 namespace GestioneSagre.Models.ViewModels.Categorie;
 public class CategoriaDetailViewModel : BaseViewModel
@@ -7,6 +8,7 @@ public class CategoriaDetailViewModel : BaseViewModel
     public string CategoriaVideo { get; set; }
     public string CategoriaStampa { get; set; }
     public string GuidFesta { get; set; }
+    public List<ProdottoViewModel> Prodotti { get; set; } = new List<ProdottoViewModel>();
 
     public static CategoriaDetailViewModel FromEntity(CategoriaEntity categoria)
     {
@@ -16,6 +18,11 @@ public class CategoriaDetailViewModel : BaseViewModel
             CategoriaVideo = categoria.CategoriaVideo,
             CategoriaStampa = categoria.CategoriaStampa,
             GuidFesta = categoria.GuidFesta,
+            Prodotti = categoria.Prodotti
+                .OrderBy(prodotto => prodotto.Id)
+                .ThenBy(prodotto => prodotto.Id)
+                .Select(prodotto => ProdottoViewModel.FromEntity(prodotto))
+                .ToList()
         };
     }
 }
--- src/GestioneSagre.Models/ViewModels/Categorie/CategoriaViewModel.cs
+++ src/GestioneSagre.Models/ViewModels/Categorie/CategoriaViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using GestioneSagre.Infrastructure.Entities;
 using GestioneSagre.Models.ViewModels.Common;
+using GestioneSagre.Models.ViewModels.Prodotti;
 
 namespace GestioneSagre.Models.ViewModels.Categorie;
 public class CategoriaViewModel : BaseViewModel
@@ -7,6 +8,7 @@ public class CategoriaViewModel : BaseViewModel
     public string CategoriaVideo { get; set; }
     public string CategoriaStampa { get; set; }
     public string GuidFesta { get; set; }
+    public List<ProdottoViewModel> Prodotti { get; set; } = new List<ProdottoViewModel>();
 
     public static CategoriaViewModel FromEntity(CategoriaEntity categoria)
     {
@@ -16,6 +18,11 @@ public class CategoriaViewModel : BaseViewModel
             CategoriaVideo = categoria.CategoriaVideo,
             CategoriaStampa = categoria.CategoriaStampa,
             GuidFesta = categoria.GuidFesta,
+            Prodotti = categoria.Prodotti
+                .OrderBy(prodotto => prodotto.Id)
+                .ThenBy(prodotto => prodotto.Id)
+                .Select(prodotto => ProdottoViewModel.FromEntity(prodotto))
+                .ToList()
         };
     }
 }
