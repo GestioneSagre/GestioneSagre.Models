4960be9a24de105d7ff7df033b3c023ab32aa2d9
Angelo Pirola <angelo9871@gmail.com>
2022-07-31 20:40:57 +0200

    Aggiunto ViewModels Impostazioni

---
+++ src/GestioneSagre.Models/ViewModels/Impostazioni/ImpostazioniDetailViewModel.cs
@@ -0,0 +1,27 @@
+﻿using GestioneSagre.Infrastructure.Entities;
+using GestioneSagre.Models.ViewModels.Common;
+
+namespace GestioneSagre.Models.ViewModels.Impostazioni;
+public class ImpostazioniDetailViewModel : BaseViewModel
+{
+    public string GuidFesta { get; set; }
+    public bool GestioneCoperti { get; set; }
+    public bool GestioneMenu { get; set; }
+    public bool StampaCarta { get; set; }
+    public bool StampaLogo { get; set; }
+    public bool StampaRicevuta { get; set; }
+
+    public static ImpostazioniDetailViewModel FromEntity(ImpostazioniEntity impostazioni)
+    {
+        return new ImpostazioniDetailViewModel
+        {
+            Id = impostazioni.Id,
+            GestioneCoperti = impostazioni.GestioneCoperti,
+            GestioneMenu = impostazioni.GestioneMenu,
+            StampaCarta = impostazioni.StampaCarta,
+            StampaLogo = impostazioni.StampaLogo,
+            StampaRicevuta = impostazioni.StampaRicevuta,
+            GuidFesta = impostazioni.GuidFesta
+        };
+    }
+}
---
+++ src/GestioneSagre.Models/ViewModels/Impostazioni/ImpostazioniListViewModel.cs
@@ -0,0 +1,5 @@
+﻿namespace GestioneSagre.Models.ViewModels.Impostazioni;
+public class ImpostazioniListViewModel
+{
+    public ListViewModel<ImpostazioniViewModel> Impostazioni { get; set; }
+}
